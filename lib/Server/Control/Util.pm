65ca86dc622b6a13c95249a528c75acc8c9a6105
Jonathan Swartz <swartz@pobox.com>
2009-08-25 10:37:04 -0400

    get rid of debug functions

--- lib/Server/Control/Util.pm
+++ lib/Server/Control/Util.pm
@@ -1,42 +1,11 @@
 package Server::Control::Util;
-use Carp qw( croak longmess );
-use Data::Dumper;
 use strict;
 use warnings;
 use base qw(Exporter);
 
 our @EXPORT_OK = qw(
-  dp
-  dps
-  dump_one_line
   trim
 );
-
-sub _dump_value_with_caller {
-    my ($value) = @_;
-
-    my $dump =
-      Data::Dumper->new( [$value] )->Indent(1)->Sortkeys(1)->Quotekeys(0)
-      ->Terse(1)->Dump();
-    my @caller = caller(1);
-    return sprintf( "[dp at %s line %d.] [%d] %s\n",
-        $caller[1], $caller[2], $$, $dump );
-}
-
-sub dp {
-    print STDERR _dump_value_with_caller(@_);
-}
-
-sub dps {
-    print STDERR longmess( _dump_value_with_caller(@_) );
-}
-
-sub dump_one_line {
-    my ($value) = @_;
-
-    return Data::Dumper->new( [$value] )->Indent(0)->Sortkeys(1)->Quotekeys(0)
-      ->Terse(1)->Dump();
-}
 
 sub trim {
     my ($str) = @_;
